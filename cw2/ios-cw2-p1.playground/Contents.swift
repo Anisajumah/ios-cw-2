def29f886e64bfbfa8e1cf460a68bc5565264031
Anisajumah <70814404+Anisajumah@users.noreply.github.com>
2020-09-12 20:34:32 +0300

    file cw2

---
+++ cw2/ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+let family = ["Anisa","Abdulrahman","Buthina"]
+var evens = [2,4,6,8,10,12]
+var grades = [98.0,97.9,98.1]
+var trueFalse = [true,true,true,false]
+print(family[0],[2])
+print(evens[0],[5])
+print(grades [0],[2])
+print(trueFalse[0],[3])
+
